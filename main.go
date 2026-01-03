4b5893a813758637deee3dc3f761869c5829f71b
spyhere <gekav3@mail.ru>
2026-01-03 16:41:13 +0200

    Add background, render carret when clicking on soundwave

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/io/semantic"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
@@ -46,12 +48,15 @@ func main() {
 	app.Main()
 }
 
-const margin = 400
+const MARGIN = 400
+const PADDING = 90
 
 func run(window *app.Window, rWaves *RenderableWaves) error {
 	theme := material.NewTheme()
+	clickable := widget.Clickable{}
 	var ops op.Ops
 	list := layout.List{}
+	carret := image.Pt(0, 0)
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
@@ -59,18 +64,36 @@ func run(window *app.Window, rWaves *RenderableWaves) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 			_ = theme
-			yMid := (e.Size.Y / 2) - margin
+			yMid := e.Size.Y / 2
+			wavesYBorder := yMid - MARGIN
 
 			rWaves.SetMaxX(e.Size.X)
 			waves := rWaves.GetRenderableWaves()
-			offset := op.Offset(image.Pt(0, margin)).Push(gtx.Ops)
+
+			bgArea := image.Rect(0, MARGIN-PADDING, e.Size.X, e.Size.Y-MARGIN+PADDING)
+			ColorBox(gtx, bgArea, color.NRGBA{R: 0x11, G: 0x77, B: 0x66, A: 0xff})
+
+			listOffset := op.Offset(image.Pt(0, MARGIN)).Push(gtx.Ops)
 			list.Layout(gtx, len(waves), func(gtx layout.Context, idx int) layout.Dimensions {
-				high := yMid - int((waves[idx][1] * float32(yMid)))
-				low := yMid - int(waves[idx][0]*float32(yMid))
-				box := ColorBox(gtx, image.Rect(0, high, 1, low), color.NRGBA{R: 0x99, B: 0xcc, A: 0xff})
-				return box
+				high := wavesYBorder - int((waves[idx][1] * float32(wavesYBorder)))
+				low := wavesYBorder - int(waves[idx][0]*float32(wavesYBorder))
+				return ColorBox(gtx, image.Rect(0, high, 1, low), color.NRGBA{G: 0x32, B: 0x55, A: 0xff})
 			})
-			offset.Pop()
+			listOffset.Pop()
+
+			if clickable.Clicked(gtx) {
+				clickHistory := clickable.History()
+				pressX := clickHistory[len(clickHistory)-1].Position.X
+				carret.X = pressX
+			}
+			activeA := op.Offset(bgArea.Min).Push(gtx.Ops)
+			clickable.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				semantic.Button.Add(gtx.Ops)
+				return layout.Dimensions{Size: image.Pt(bgArea.Dx(), bgArea.Dy())}
+			})
+			activeA.Pop()
+
+			ColorBox(gtx, image.Rect(carret.X, 0, carret.X+1, e.Size.Y), color.NRGBA{R: 0xff, G: 0xdd, B: 0xdd, A: 0xff})
 			e.Frame(gtx.Ops)
 		}
 	}
