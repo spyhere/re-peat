f95ca1ba27381780ee4b183ae38bc8484b646467
spyhere <gekav3@mail.ru>
2026-01-04 18:48:09 +0200

    Create Player wrapper around oto's one

--- go.mod
+++ go.mod
@@ -4,16 +4,16 @@ go 1.25.4
 
 require (
 	gioui.org v0.9.0
-	github.com/hajimehoshi/oto v1.0.2
+	github.com/ebitengine/oto/v3 v3.4.0
 	github.com/tosone/minimp3 v1.0.2
 )
 
 require (
 	gioui.org/shader v1.0.8 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
 	github.com/go-text/typesetting v0.3.0 // indirect
 	golang.org/x/exp/shiny v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
 	golang.org/x/image v0.26.0 // indirect
-	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 )
--- player.go
+++ player.go
@@ -1,27 +1,118 @@
 package main
 
 import (
-	"time"
+	"bytes"
+	"errors"
+	"io"
 
-	"github.com/hajimehoshi/oto"
+	"github.com/ebitengine/oto/v3"
 	"github.com/tosone/minimp3"
 )
 
-func player(dec *minimp3.Decoder, data []byte) error {
+type Player struct {
+	player  *oto.Player
+	cReader *countingReader
+	// Amount of PCM bytes
+	dataLen int
+}
+
+func (p *Player) Play() {
+	p.player.Play()
+}
+
+func (p *Player) Pause() {
+	p.player.Pause()
+	p.cReader.isActive = false
+}
+
+func (p *Player) SetVolume(volume float64) {
+	p.player.SetVolume(volume)
+}
+
+const BYTES_PER_SAMPLE int64 = 2
+
+func (p *Player) Search(offset float64) (int64, error) {
+	value := int64(offset * float64(p.dataLen) / 100.0)
+	value -= value % BYTES_PER_SAMPLE
+	return p.player.Seek(value, io.SeekStart)
+}
+
+func (p *Player) BufferedSize() int {
+	return p.player.BufferedSize()
+}
+
+func (p *Player) GetReadAmount() int64 {
+	return p.cReader.bytes - int64(p.player.BufferedSize())
+}
+
+func (p *Player) WaitWhenReady() bool {
+	return <-p.cReader.ready
+}
+
+func (p *Player) IsDoneCh() chan bool {
+	return p.cReader.done
+}
+
+func NewPlayer(dec *minimp3.Decoder, data []byte) (*Player, error) {
 	var err error
-	var context *oto.Context
-	if context, err = oto.NewContext(dec.SampleRate, dec.Channels, 2, 1024); err != nil {
-		return err
+	op := &oto.NewContextOptions{
+		SampleRate:   dec.SampleRate,
+		ChannelCount: dec.Channels,
+		Format:       oto.FormatSignedInt16LE,
 	}
-
-	var player = context.NewPlayer()
-	player.Write(data)
-
-	<-time.After(time.Second)
-
-	dec.Close()
-	if err = player.Close(); err != nil {
-		return err
+	otoCtx, readyChan, err := oto.NewContext(op)
+	if err != nil {
+		return &Player{}, err
 	}
-	return nil
+	<-readyChan
+	cReader := &countingReader{
+		r:     bytes.NewReader(data),
+		ready: make(chan bool),
+		done:  make(chan bool),
+	}
+	player := otoCtx.NewPlayer(cReader)
+	return &Player{
+		player:  player,
+		cReader: cReader,
+		dataLen: len(data),
+	}, nil
+}
+
+type countingReader struct {
+	r *bytes.Reader
+	// What's the current offset
+	bytes int64
+	// Is reading at the moment
+	isActive bool
+	// User of this reader has started to read
+	ready chan bool
+	// EOF is beaing reached
+	done chan bool
+}
+
+func (c *countingReader) Read(p []byte) (int, error) {
+	if !c.isActive {
+		c.isActive = true
+		c.ready <- true
+	}
+	// Should read after the check otherwise it can be janky sometimes
+	n, err := c.r.Read(p)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			c.done <- true
+			c.isActive = false
+		}
+		return 0, err
+	}
+	c.bytes += int64(n)
+	return n, err
+}
+
+func (c *countingReader) Seek(offset int64, whence int) (int64, error) {
+	c.bytes = offset
+	return c.r.Seek(offset, whence)
+}
+
+func (c *countingReader) Stop() {
+	c.isActive = false
 }
