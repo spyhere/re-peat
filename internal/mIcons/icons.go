af46d9b0067a914ee00e4e9bb33055ea5557203f
spyhere <gekav3@mail.ru>
2026-01-26 16:16:17 +0200

    Create icons package to load material design icons

---
+++ internal/mIcons/icons.go
@@ -0,0 +1,20 @@
+package micons
+
+import (
+	"log"
+
+	"gioui.org/widget"
+	"golang.org/x/exp/shiny/materialdesign/icons"
+)
+
+func newIcon(i []byte) *widget.Icon {
+	wI, err := widget.NewIcon(i)
+	if err != nil {
+		log.Println(err)
+	}
+	return wI
+}
+
+var (
+	ContentAddCircle = newIcon(icons.ContentAddCircle)
+)
