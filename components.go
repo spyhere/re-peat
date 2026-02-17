657bf1e41be08f8ee591cca34244f145d8498a46
spyhere <gekav3@mail.ru>
2026-02-17 18:52:39 +0200

    Apply materialUI specs to segmented buttons

--- components.go
+++ components.go
@@ -2,57 +2,119 @@ package main
 
 import (
 	"image"
+	"image/color"
 
+	"gioui.org/font"
 	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/unit"
 	"gioui.org/widget/material"
 	"github.com/spyhere/re-peat/internal/common"
+	micons "github.com/spyhere/re-peat/internal/mIcons"
 	"github.com/spyhere/re-peat/internal/ui/theme"
 )
 
-const outlineWidth unit.Dp = 1
-const minPadDP unit.Dp = 12
+type segmentedButtSpecs struct {
+	height        unit.Dp
+	outline       unit.Dp
+	minPad        unit.Dp
+	iconSize      unit.Dp
+	elementsGap   unit.Dp
+	fontFace      font.Typeface
+	fontWeight    font.Weight
+	fontSize      unit.Sp
+	fontLineHeigt unit.Sp
+}
 
-// TODO: make helper to make it 50% according to the height of the button
-var cornerL = theme.CornerR(0, 35, 35, 0)
-var cornerR = theme.CornerR(35, 0, 0, 35)
+var segButtSpecs segmentedButtSpecs = segmentedButtSpecs{
+	height:        40,
+	outline:       1,
+	minPad:        16,
+	iconSize:      18,
+	elementsGap:   8,
+	fontFace:      "Roboto",
+	fontWeight:    500,
+	fontSize:      14,
+	fontLineHeigt: 20,
+}
 
-func groupedButtons(gtx layout.Context, th *theme.RepeatTheme, buttons *buttons) {
-	var xOffset int
-	minPad := gtx.Dp(minPadDP)
-	buttonsLayout := common.MakeMacro(gtx.Ops, func() {
-		for idx, it := range buttons.arr {
-			var textDim layout.Dimensions
-			textOp := common.MakeMacro(gtx.Ops, func() {
-				gtx.Constraints.Min = image.Point{}
-				textBody2 := material.Body2(th.Theme, it.name)
-				textDim = textBody2.Layout(gtx)
-			})
+type buttonText struct {
+	width int
+	op    op.CallOp
+}
 
-			common.OffsetBy(gtx, image.Pt(xOffset, 0), func() {
-				buttArea := image.Rect(0, 0, textDim.Size.X+minPad*2, textDim.Size.Y+minPad*2)
-				var corner theme.CornerRadii
-				if idx == 0 {
-					corner = cornerL
-				} else if idx == len(buttons.arr)-1 {
-					corner = cornerR
-				}
-				common.ColorBox(gtx, common.Box{
-					Size:    buttArea,
-					Color:   th.Palette.Editor.Bg,
-					R:       corner,
-					StrokeW: outlineWidth,
-					StrokeC: th.Palette.GrButtons.Outline,
-				})
-				common.OffsetBy(gtx, image.Pt(int(minPad), (buttArea.Dy()-textDim.Size.Y)/2), func() {
-					textOp.Add(gtx.Ops)
-				})
-				common.RegisterTag(gtx, &it.tag, buttArea)
-			})
-			xOffset += textDim.Size.X + int(minPad)*2
+var buttTextOps [3]buttonText = [3]buttonText{}
+
+func groupedButtons(gtx layout.Context, th *theme.RepeatTheme, selectedT tab, buttons *buttons) layout.Dimensions {
+	var maxDim layout.Dimensions
+	for idx, it := range buttons.arr {
+		var textDim layout.Dimensions
+		textOp := common.MakeMacro(gtx.Ops, func() {
+			gtx.Constraints.Min = image.Point{}
+			textBody2 := material.Body2(th.Theme, it.name)
+			if buttons.arr[idx].tab == selectedT {
+				textBody2.Color = th.Palette.SegButtons.SelText
+			} else {
+				textBody2.Color = th.Palette.SegButtons.UnSelText
+			}
+			textBody2.Font.Typeface = segButtSpecs.fontFace
+			textBody2.Font.Weight = segButtSpecs.fontWeight
+			textBody2.TextSize = segButtSpecs.fontSize
+			textBody2.LineHeight = segButtSpecs.fontLineHeigt
+			textDim = textBody2.Layout(gtx)
+		})
+		buttTextOps[idx] = buttonText{width: textDim.Size.X, op: textOp}
+		if textDim.Size.X > maxDim.Size.X {
+			maxDim = textDim
 		}
-	})
-	common.OffsetBy(gtx, image.Pt(-xOffset/2, 0), func() {
-		buttonsLayout.Add(gtx.Ops)
-	})
+	}
+	iconSize, elementsGap, containerH :=
+		gtx.Dp(segButtSpecs.iconSize), gtx.Dp(segButtSpecs.elementsGap), gtx.Dp(segButtSpecs.height)
+	maxDim.Size.X += iconSize + elementsGap + gtx.Dp(segButtSpecs.minPad)*2
+
+	containerHHalf := containerH / 2
+	var xOffset int
+	for idx, it := range buttTextOps {
+		curButt := buttons.arr[idx]
+		common.OffsetBy(gtx, image.Pt(xOffset, 0), func() {
+			var corner theme.CornerRadii
+			if idx == 0 {
+				corner = theme.CornerR(0, containerHHalf, containerHHalf, 0)
+			} else if idx == len(buttons.arr)-1 {
+				corner = theme.CornerR(containerHHalf, 0, 0, containerHHalf)
+			}
+			var col color.NRGBA
+			if curButt.tab == selectedT {
+				col = th.Palette.SegButtons.Selected
+			}
+			buttArea := image.Rect(0, 0, maxDim.Size.X, containerH)
+			common.ColorBox(gtx, common.Box{
+				Size:    buttArea,
+				Color:   col,
+				R:       corner,
+				StrokeW: segButtSpecs.outline,
+				StrokeC: th.Palette.SegButtons.Outline,
+			})
+			common.RegisterTag(gtx, &curButt.tag, buttArea)
+
+			y := (containerH - maxDim.Size.Y) / 2
+			if curButt.tab == selectedT {
+				x := (maxDim.Size.X - it.width - iconSize - elementsGap) / 2
+				common.OffsetBy(gtx, image.Pt(x, y), func() {
+					gtx.Constraints.Min.X = iconSize
+					micons.Check.Layout(gtx, th.Palette.SegButtons.SelText)
+					common.OffsetBy(gtx, image.Pt(iconSize+elementsGap, 0), func() {
+						it.op.Add(gtx.Ops)
+					})
+				})
+			} else {
+				x := (maxDim.Size.X - it.width) / 2
+				common.OffsetBy(gtx, image.Pt(x, y), func() {
+					it.op.Add(gtx.Ops)
+				})
+			}
+		})
+		xOffset += maxDim.Size.X
+	}
+	return layout.Dimensions{Size: image.Pt(xOffset, containerH)}
 }
--- internal/ui/theme/palette.go
+++ internal/ui/theme/palette.go
@@ -4,8 +4,11 @@ import "image/color"
 
 var repeatPalette = palette{
 	Backdrop: argb(0xdd000000),
-	GrButtons: grButtonsPalette{
-		Outline: rgb(0x79747E),
+	SegButtons: segButtonsPalette{
+		Outline:   rgb(0x79747E),
+		Selected:  rgb(0xE8DEF8),
+		SelText:   rgb(0x4A4458),
+		UnSelText: rgb(0x1D1B20),
 	},
 	Editor: editorPalette{
 		Bg:        tan,
@@ -34,13 +37,16 @@ var (
 )
 
 type palette struct {
-	Backdrop  color.NRGBA
-	GrButtons grButtonsPalette
-	Editor    editorPalette
+	Backdrop   color.NRGBA
+	SegButtons segButtonsPalette
+	Editor     editorPalette
 }
 
-type grButtonsPalette struct {
-	Outline color.NRGBA
+type segButtonsPalette struct {
+	Outline   color.NRGBA
+	Selected  color.NRGBA
+	SelText   color.NRGBA
+	UnSelText color.NRGBA
 }
 
 type editorPalette struct {
