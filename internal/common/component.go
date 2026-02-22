190898e9ecbdc7c056f226421e2410597778fe72
spyhere <gekav3@mail.ru>
2026-02-22 15:35:02 +0200

    Add material's ink animation functionality for DrawBox

--- internal/common/component.go
+++ internal/common/component.go
@@ -3,20 +3,133 @@ package common
 import (
 	"image"
 	"image/color"
+	"math"
 
 	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
+	"gioui.org/widget"
 	"github.com/spyhere/re-peat/internal/ui/theme"
 )
 
+func colPremul(c, a byte) byte {
+	return byte(uint16(c) * uint16(a) / 255)
+}
+
+// This is a copy-pasted function from gio's "widget/material/button.go"
+func drawInk(gtx layout.Context, c widget.Press) {
+	// duration is the number of seconds for the
+	// completed animation: expand while fading in, then
+	// out.
+	const (
+		expandDuration = float32(0.5)
+		fadeDuration   = float32(0.9)
+	)
+
+	now := gtx.Now
+
+	t := float32(now.Sub(c.Start).Seconds())
+
+	end := c.End
+	if end.IsZero() {
+		// If the press hasn't ended, don't fade-out.
+		end = now
+	}
+
+	endt := float32(end.Sub(c.Start).Seconds())
+
+	// Compute the fade-in/out position in [0;1].
+	var alphat float32
+	{
+		var haste float32
+		if c.Cancelled {
+			// If the press was cancelled before the inkwell
+			// was fully faded in, fast forward the animation
+			// to match the fade-out.
+			if h := 0.5 - endt/fadeDuration; h > 0 {
+				haste = h
+			}
+		}
+		// Fade in.
+		half1 := t/fadeDuration + haste
+		if half1 > 0.5 {
+			half1 = 0.5
+		}
+
+		// Fade out.
+		half2 := float32(now.Sub(end).Seconds())
+		half2 /= fadeDuration
+		half2 += haste
+		if half2 > 0.5 {
+			// Too old.
+			return
+		}
+
+		alphat = half1 + half2
+	}
+
+	// Compute the expand position in [0;1].
+	sizet := t
+	if c.Cancelled {
+		// Freeze expansion of cancelled presses.
+		sizet = endt
+	}
+	sizet /= expandDuration
+
+	// Animate only ended presses, and presses that are fading in.
+	if !c.End.IsZero() || sizet <= 1.0 {
+		gtx.Execute(op.InvalidateCmd{})
+	}
+
+	if sizet > 1.0 {
+		sizet = 1.0
+	}
+
+	if alphat > .5 {
+		// Start fadeout after half the animation.
+		alphat = 1.0 - alphat
+	}
+	// Twice the speed to attain fully faded in at 0.5.
+	t2 := alphat * 2
+	// Beziér ease-in curve.
+	alphaBezier := t2 * t2 * (3.0 - 2.0*t2)
+	sizeBezier := sizet * sizet * (3.0 - 2.0*sizet)
+	size := gtx.Constraints.Min.X
+	if h := gtx.Constraints.Min.Y; h > size {
+		size = h
+	}
+	// Cover the entire constraints min rectangle and
+	// apply curve values to size and color.
+	size = int(float32(size) * 2 * float32(math.Sqrt(2)) * sizeBezier)
+	alpha := 0.7 * alphaBezier
+	const col = 0.8
+	ba, bc := byte(alpha*0xff), byte(col*0xff)
+	rgba := color.NRGBA{
+		A: ba,
+		R: colPremul(bc, ba),
+		G: colPremul(bc, ba),
+		B: colPremul(bc, ba),
+	}
+	ink := paint.ColorOp{Color: rgba}
+	ink.Add(gtx.Ops)
+	rr := size / 2
+	defer op.Offset(c.Position.Add(image.Point{
+		X: -rr,
+		Y: -rr,
+	})).Push(gtx.Ops).Pop()
+	defer clip.UniformRRect(image.Rectangle{Max: image.Pt(size, size)}, rr).Push(gtx.Ops).Pop()
+	paint.PaintOp{}.Add(gtx.Ops)
+}
+
 type Box struct {
-	Size    image.Rectangle
-	Color   color.NRGBA
-	R       theme.CornerRadii
-	StrokeC color.NRGBA
-	StrokeW unit.Dp
+	Size      image.Rectangle
+	Color     color.NRGBA
+	R         theme.CornerRadii
+	StrokeC   color.NRGBA
+	StrokeW   unit.Dp
+	Clickable *widget.Clickable
 }
 
 func DrawBox(gtx layout.Context, b Box) layout.Dimensions {
@@ -25,7 +138,17 @@ func DrawBox(gtx layout.Context, b Box) layout.Dimensions {
 	rrectStack := rrect.Push(gtx.Ops)
 	paint.ColorOp{Color: b.Color}.Add(gtx.Ops)
 	paint.PaintOp{}.Add(gtx.Ops)
+	if b.Clickable != nil {
+		b.Clickable.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			return layout.Dimensions{Size: rrect.Rect.Max}
+		})
+		for _, it := range b.Clickable.History() {
+			gtx.Constraints.Min = rrect.Rect.Max
+			drawInk(gtx, it)
+		}
+	}
 	rrectStack.Pop()
+
 	if b.StrokeW != 0 {
 		stroke := clip.Stroke{
 			Path:  rrect.Path(gtx.Ops),
