d78536f3a219e425cb31ce40576ea6d6c4a6a0ee
spyhere <gekav3@mail.ru>
2026-02-27 21:05:16 +0200

    Draw Divider unser Search component

--- internal/markersView/layout.go
+++ internal/markersView/layout.go
@@ -11,14 +11,22 @@ import (
 var searchable = common.Searchable{}
 
 func (m *MarkersView) Layout(gtx layout.Context) layout.Dimensions {
-	common.OffsetBy(gtx, image.Pt(0, 400), func() {
+	var searchDims layout.Dimensions
+	common.OffsetBy(gtx, image.Pt(0, 200), func() {
 		common.CenteredX(gtx, func() layout.Dimensions {
-			return common.DrawSearch(gtx, m.th, common.SProps{
+			searchDims = common.DrawSearch(gtx, m.th, common.SProps{
 				DefaultText: "Название маркера...",
 				Searchable:  &searchable,
 			})
+			return searchDims
 		})
 	})
+	common.OffsetBy(gtx, image.Pt(0, 200+searchDims.Size.Y+20), func() {
+		common.DrawDivider(gtx, m.th, common.DividerProps{
+			Inset: common.DividerMiddleInset,
+		})
+	})
+
 	if searchable.IsHovered() {
 		if searchable.IsFocused() {
 			common.SetCursor(gtx, pointer.CursorText)
