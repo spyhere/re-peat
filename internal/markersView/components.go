cdd2aa9ada80e4448045e5c58f06103fda53e31a
spyhere <gekav3@mail.ru>
2026-03-08 13:28:04 +0200

    Create component to draw input fields for marker's dialog

--- internal/markersView/components.go
+++ internal/markersView/components.go
@@ -22,3 +22,77 @@ func drawClickableIcon(gtx layout.Context, icon *widget.Icon, iconSize unit.Dp, 
 	})
 	return icon.Layout(gtx, iconC)
 }
+
+type drawMarkerDialogSizeSpecs struct {
+	fieldsYMargin unit.Dp
+	fieldsXMargin unit.Dp
+	fieldW        unit.Dp
+	gap           unit.Dp
+}
+
+var drawMarkerDialogSpecs = drawMarkerDialogSizeSpecs{
+	fieldsYMargin: 10,
+	fieldsXMargin: 10,
+	fieldW:        270,
+	gap:           20,
+}
+
+type markerDialogFieldsProps struct {
+	name         *common.Inputable
+	time         *common.Inputable
+	tags         *common.Inputable
+	chips        []string
+	totalSeconds float64
+}
+
+func drawMarkerDialogFields(gtx layout.Context, th *theme.RepeatTheme, props markerDialogFieldsProps) layout.Dimensions {
+	s := drawMarkerDialogSpecs
+	inset := layout.Inset{Top: s.fieldsYMargin, Bottom: s.fieldsYMargin, Left: s.fieldsXMargin, Right: s.fieldsXMargin}
+	return inset.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+		gapPx := gtx.Dp(s.gap)
+		gtx.Constraints.Max.X = gtx.Constraints.Min.X
+		gtx.Constraints.Min.X = gtx.Constraints.Max.X
+		fieldW := gtx.Dp(s.fieldW)
+		return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					gtx.Constraints.Max.X = fieldW
+					inputDims := common.DrawInputField(gtx, th, common.InputFieldProps{
+						Base: common.InputFieldBase{
+							LabelText: "Имя",
+							Inputable: props.name,
+						},
+						MaxLen:      20,
+						Placeholder: "Новый маркер...",
+					})
+					inputDims.Size.Y += gapPx
+					return inputDims
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					gtx.Constraints.Max.X = fieldW
+					inputDims := common.DrawInputField(gtx, th, common.InputFieldProps{
+						Base: common.InputFieldBase{
+							LabelText: "Время",
+							Inputable: props.time,
+						},
+						MaxLen:      7,
+						Placeholder: common.FormatSeconds(props.totalSeconds),
+					})
+					inputDims.Size.Y += gapPx
+					return inputDims
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					gtx.Constraints.Max.X = fieldW
+					return common.DrawCombobox(gtx, th, common.ComboboxProps{
+						Base: common.InputFieldBase{
+							LabelText: "Категории",
+							Inputable: props.tags,
+						},
+						Chips:  props.chips,
+						MaxLen: 20,
+					})
+				}),
+			)
+		})
+	})
+}
