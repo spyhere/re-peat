6c14502ef8ec04326dd74b6384beb3edae1d0481
spyhere <gekav3@mail.ru>
2026-04-13 23:34:40 +0300

    Add string version to AudioMeta fields

--- internal/audio/audio.go
+++ internal/audio/audio.go
@@ -1,7 +1,10 @@
 package audio
 
 import (
+	"fmt"
 	"math"
+
+	"github.com/spyhere/re-peat/internal/common"
 )
 
 func NewAudioMeta(sampleRate, channels int, maxSamples int) AudioMeta {
@@ -33,4 +36,32 @@ func (a AudioMeta) GetSamplesFromSeconds(seconds float64) int {
 }
 func (a AudioMeta) GetSamplesAmount() int {
 	return a.MonoSamplesLen * a.Channels
+}
+
+func (a AudioMeta) SecondsString() string {
+	if a.Seconds == 0.0 {
+		return ""
+	}
+	return common.FormatSeconds(a.Seconds)
+}
+
+func (a AudioMeta) ChannelsString() string {
+	if a.Channels == 0 {
+		return ""
+	}
+	switch a.Channels {
+	case 1:
+		return "Mono"
+	case 2:
+		return "Stereo"
+	default:
+		panic("unreachable")
+	}
+}
+
+func (a AudioMeta) SampleRateString() string {
+	if a.SampleRate == 0 {
+		return ""
+	}
+	return fmt.Sprintf("%.1f kHz", float64(a.SampleRate)/1000)
 }
