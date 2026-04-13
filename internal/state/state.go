e0153780b2a6be6c1fc48f253dc4a100bf161170
spyhere <gekav3@mail.ru>
2026-04-13 21:44:06 +0300

    Create App state struct

---
+++ internal/state/state.go
@@ -0,0 +1,105 @@
+package state
+
+import (
+	"errors"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/x/explorer"
+	"github.com/spyhere/re-peat/internal/audio"
+	"github.com/spyhere/re-peat/internal/filemanager"
+	p "github.com/spyhere/re-peat/internal/player"
+	tm "github.com/spyhere/re-peat/internal/timeMarkers"
+)
+
+const defaultPlayerVol = 0.5
+
+func NewAppState(window *app.Window) AppState {
+	return AppState{
+		fileManager: filemanager.NewFileManager(window),
+		TimeMarkers: tm.NewTimeMarkers(),
+	}
+}
+
+type AppState struct {
+	fileManager *filemanager.FileManager
+	LoadedFile  string
+	Player      *p.Player
+	MonoSamples []float32
+	AudioMeta   audio.AudioMeta
+	TimeMarkers tm.TimeMarkers
+	isChoosing  bool
+	isLoading   bool
+	err         error
+}
+
+func (a *AppState) IsChoosing() bool {
+	return a.isChoosing
+}
+func (a *AppState) IsLoading() bool {
+	return a.isLoading
+}
+
+func (a *AppState) HasAudioLoaded() bool {
+	return a.LoadedFile != ""
+}
+
+func (a *AppState) GetError() error {
+	err := a.err
+	a.err = nil
+	return err
+}
+
+func (a *AppState) Reset() {
+	a.LoadedFile = ""
+	a.err = nil
+	if a.Player != nil {
+		a.Player.Reset()
+	}
+	a.MonoSamples = a.MonoSamples[:0]
+	a.TimeMarkers.MarkAllDead()
+	a.TimeMarkers.DeleteDead()
+}
+
+func (a *AppState) AudioLoad() {
+	a.isChoosing = true
+	a.fileManager.Load(func(filePath string, err error) {
+		a.isChoosing = false
+		if err != nil {
+			if !errors.Is(err, explorer.ErrUserDecline) {
+				a.err = err
+			}
+			return
+		}
+		if a.LoadedFile == filePath {
+			return
+		}
+		a.isLoading = true
+		a.Reset()
+		monoSamples, audioMeta, err := audio.LoadMonoSamples(filePath)
+		if err != nil {
+			a.err = err
+			return
+		}
+		file, err := os.Open(filePath)
+		if err != nil {
+			a.err = err
+			return
+		}
+		if a.Player == nil {
+			a.Player = p.NewPlayer()
+			a.Player.SetVolume(defaultPlayerVol)
+		}
+		// NOTE: Is it safe to decode audio once?
+		a.Player.SetAudio(file)
+
+		a.MonoSamples = monoSamples
+		a.AudioMeta = audioMeta
+		a.LoadedFile = filePath
+
+		a.isLoading = false
+	}, ".mp3", ".wav", ".flac")
+}
+func (a *AppState) MarkersLoad()   {}
+func (a *AppState) MarkersSave()   {}
+func (a *AppState) MarkersSaveAs() {}
