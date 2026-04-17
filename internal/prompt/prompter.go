f3819a3557b96a3cfb5cc01ff3502d7b34096b08
spyhere <gekav3@mail.ru>
2026-04-17 19:36:19 +0300

    Make Prompter expose Dialog

--- internal/prompt/prompter.go
+++ internal/prompt/prompter.go
@@ -10,41 +10,41 @@ import (
 func NewPrompter(th *theme.RepeatTheme) Prompter {
 	return Prompter{
 		th:     th,
-		dialog: common.Dialog{},
+		Dialog: common.Dialog{},
 		ch:     make(chan bool),
 	}
 }
 
 type Prompter struct {
 	th     *theme.RepeatTheme
-	dialog common.Dialog
+	Dialog common.Dialog
 	ch     chan bool
 }
 
 // This blocks goroutine
 func (p *Prompter) Ask(title, question string) bool {
-	p.dialog.Basic(p.th, title, func(gtx layout.Context) layout.Dimensions {
+	p.Dialog.Basic(p.th, title, func(gtx layout.Context) layout.Dimensions {
 		return material.Body2(p.th.Theme, question).Layout(gtx)
 	})
-	p.dialog.Show()
+	p.Dialog.Show()
 	return <-p.ch
 }
 
 // Should be at the end of the frame, since it uses dialog
 func (p *Prompter) Layout(gtx layout.Context) {
-	p.dialog.Update(gtx)
-	if p.dialog.IsCanceled() {
-		p.dialog.Hide()
+	p.Dialog.Update(gtx)
+	if p.Dialog.IsCanceled() {
+		p.Dialog.Hide()
 		p.ch <- false
 	}
-	if p.dialog.IsConfirmed() {
-		p.dialog.Hide()
+	if p.Dialog.IsConfirmed() {
+		p.Dialog.Hide()
 		p.ch <- true
 	}
 
-	p.dialog.Layout(gtx)
+	p.Dialog.Layout(gtx)
 
-	if cursor, ok := p.dialog.GetCursorType(); ok {
+	if cursor, ok := p.Dialog.GetCursorType(); ok {
 		cursor.Add(gtx.Ops)
 	}
 }
